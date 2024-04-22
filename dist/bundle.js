835483ce70cde8c98d3cbcee962dfa0daf60a94f
siyu <csyslow@gmail.com>
2024-04-22 15:35:14 -0400

    module bundling finished

--- dist/bundle.js
+++ dist/bundle.js
@@ -14,9 +14,49 @@
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n__webpack_require__(/*! ./style/index.less */ \"./src/style/index.less\");\nconsole.log(123);\n\n//# sourceURL=webpack://part3/./src/index.ts?");
+eval("\n\nvar __importDefault = this && this.__importDefault || function (mod) {\n  return mod && mod.__esModule ? mod : {\n    \"default\": mod\n  };\n};\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\n__webpack_require__(/*! ./style/index.less */ \"./src/style/index.less\");\n// import Food from './modules/Food';\n// import ScoreBoard from './modules/ScoreBoard';\n// import Snake from './modules/Snake';\nconst GameControl_1 = __importDefault(__webpack_require__(/*! ./modules/GameControl */ \"./src/modules/GameControl.ts\"));\nnew GameControl_1.default();\n\n//# sourceURL=webpack://part3/./src/index.ts?");
+
+/***/ }),
+
+/***/ "./src/modules/Food.ts":
+/*!*****************************!*\
+  !*** ./src/modules/Food.ts ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nclass Food {\n  constructor() {\n    this.element = document.getElementById('food'); //有可能取不到为null，！表示不可能为空，让检查器忽略\n  }\n  // get food current x-position\n  get x() {\n    return this.element.offsetLeft;\n  }\n  //get food current y-position\n  get y() {\n    return this.element.offsetTop;\n  }\n  //change food position randomly\n  changePosition() {\n    //offsets must be multiple of 10px\n    this.element.style.left = Math.round(Math.random() * 29) * 10 + \"px\";\n    this.element.style.top = Math.round(Math.random() * 29) * 10 + \"px\";\n  }\n}\nexports[\"default\"] = Food;\n\n//# sourceURL=webpack://part3/./src/modules/Food.ts?");
+
+/***/ }),
+
+/***/ "./src/modules/GameControl.ts":
+/*!************************************!*\
+  !*** ./src/modules/GameControl.ts ***!
+  \************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("\n\nvar __importDefault = this && this.__importDefault || function (mod) {\n  return mod && mod.__esModule ? mod : {\n    \"default\": mod\n  };\n};\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nconst Snake_1 = __importDefault(__webpack_require__(/*! ./Snake */ \"./src/modules/Snake.ts\"));\nconst Food_1 = __importDefault(__webpack_require__(/*! ./Food */ \"./src/modules/Food.ts\"));\nconst ScoreBoard_1 = __importDefault(__webpack_require__(/*! ./ScoreBoard */ \"./src/modules/ScoreBoard.ts\"));\nclass GameControl {\n  constructor() {\n    this.direction = '';\n    this.gameOver = false;\n    //get all objects\n    this.snake = new Snake_1.default();\n    this.food = new Food_1.default();\n    this.scoreBoard = new ScoreBoard_1.default();\n    //创建对象后直接开始游戏\n    this.gameInit();\n  }\n  // start the game\n  gameInit() {\n    //bind keydown control\n    //this因为是addEventListener调用的，this指向的是document而不是当前GC对象\n    //bind()相当于创建了一个新函数，把原来的this绑定为当前GC对象的this\n    document.addEventListener(\"keydown\", this.keydownHandler.bind(this));\n    //只调一次\n    this.moveSnake();\n  }\n  //keydown handler\n  keydownHandler(event) {\n    //check keys before assignment\n    this.direction = event.key;\n  }\n  // control snake direction\n  moveSnake() {\n    //get snake's current offsets\n    let new_x = this.snake.x;\n    let new_y = this.snake.y;\n    switch (this.direction) {\n      case 'ArrowUp':\n        new_y -= 10;\n        break;\n      case 'ArrowDown':\n        new_y += 10;\n        break;\n      case 'ArrowLeft':\n        new_x -= 10;\n        break;\n      case 'ArrowRight':\n        new_x += 10;\n        break;\n    }\n    this.checkEat(new_x, new_y);\n    //check if the snake hits the bound\n    try {\n      this.snake.x = new_x;\n      this.snake.y = new_y;\n    } catch (error) {\n      alert(error.message + ' The game is over');\n      this.gameOver = true;\n    }\n    //定时调用,moveSnake执行到末尾的时候又开启定时器执行moveSnake\n    this.gameOver || setTimeout(this.moveSnake.bind(this), 300 - (this.scoreBoard.level - 1) * 30);\n  }\n  //check if the snake got the food\n  checkEat(x, y) {\n    if (x === this.food.x && y === this.food.y) {\n      //operations after got the food\n      this.food.changePosition();\n      this.scoreBoard.addScore();\n      this.snake.addBody();\n    }\n  }\n}\nexports[\"default\"] = GameControl;\n\n//# sourceURL=webpack://part3/./src/modules/GameControl.ts?");
+
+/***/ }),
+
+/***/ "./src/modules/ScoreBoard.ts":
+/*!***********************************!*\
+  !*** ./src/modules/ScoreBoard.ts ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nclass ScoreBoard {\n  //10是默认值，如果有创建对象的时候传新的maxLv则用参数的，否则都是默认10\n  constructor(maxLv = 10, levelUpScore = 10) {\n    this.score = 0;\n    this.level = 1;\n    this.scoreElem = document.getElementById('score');\n    this.levelElem = document.getElementById('level');\n    this.maxLv = maxLv;\n    this.levelUpScore = levelUpScore;\n  }\n  //add score\n  addScore() {\n    this.score++;\n    this.scoreElem.innerHTML = this.score + \"\";\n    //level up every 10 points\n    if (this.score % this.levelUpScore === 0) {\n      this.levelUp();\n    }\n  }\n  //minus score\n  minusScore() {\n    this.score--;\n    this.scoreElem.innerHTML = this.score + \"\";\n  }\n  // level up\n  levelUp() {\n    if (this.level < this.maxLv) {\n      this.level++;\n      this.levelElem.innerHTML = this.level + \"\";\n    }\n  }\n}\nexports[\"default\"] = ScoreBoard;\n\n//# sourceURL=webpack://part3/./src/modules/ScoreBoard.ts?");
+
+/***/ }),
+
+/***/ "./src/modules/Snake.ts":
+/*!******************************!*\
+  !*** ./src/modules/Snake.ts ***!
+  \******************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));\nclass Snake {\n  constructor() {\n    this.snakeHead = document.querySelector('#snake > div');\n    this.snakeBodies = document.getElementById('snake').getElementsByTagName('div'); //返回的是collection，queryselectall会返回nodelist吗，不是动态的\n    this.snakeElem = document.getElementById(\"snake\");\n  }\n  //get snake head offsets\n  get x() {\n    return this.snakeHead.offsetLeft;\n  }\n  get y() {\n    return this.snakeHead.offsetTop;\n  }\n  set x(value) {\n    if (this.x === value) {\n      return;\n    }\n    //hit the bound\n    if (value < 0 || value > 290) {\n      throw new Error('the snake hit the bound.');\n    }\n    //处理掉头-水平方向\n    if (this.snakeBodies[1] && this.snakeBodies[1].offsetLeft === value) {\n      //第二节存在且第二个和头重合即掉头\n      if (value > this.x) {\n        //这时候向右走，如果这时候发生掉头应该继续向左走\n        value = this.x - 10; //反方向（向左）修正方向\n      } else {\n        value = this.x + 10;\n      }\n    }\n    //move bodies\n    this.moveBody();\n    //move head\n    this.snakeHead.style.left = value + 'px';\n    this.checkOverlap();\n  }\n  set y(value) {\n    if (this.y === value) {\n      return;\n    }\n    //hit the bound\n    if (value < 0 || value > 290) {\n      throw new Error('The snake hits the wall!');\n    }\n    //处理掉头-垂直方向\n    if (this.snakeBodies[1] && this.snakeBodies[1].offsetTop === value) {\n      //第二节存在且第二个和头重合即掉头\n      if (value > this.y) {\n        //这时候向右走，如果这时候发生掉头应该继续向左走\n        value = this.y - 10; //反方向（向左）修正方向\n      } else {\n        value = this.y + 10;\n      }\n    }\n    //move bodies\n    this.moveBody();\n    //move head\n    this.snakeHead.style.top = value + 'px';\n    this.checkOverlap();\n  }\n  //add snake bodies\n  addBody() {\n    this.snakeElem.insertAdjacentHTML(\"beforeend\", \"<div></div>\");\n  }\n  //body move\n  moveBody() {\n    //将后边的身体设置为前面身体的位置，从后往前改， 如4 = 3. 3 = 2。 2 = 1 。。\n    for (let i = this.snakeBodies.length - 1; i > 0; i--) {\n      //倒数第二个\n      //collection里的元素类型为Element，offsetXxx是HTMLElement 类型，也是Element的子类，因此要断言\n      let next_x = this.snakeBodies[i - 1].offsetLeft;\n      let next_y = this.snakeBodies[i - 1].offsetTop;\n      //reset current body\n      this.snakeBodies[i].style.left = next_x + 'px';\n      this.snakeBodies[i].style.top = next_y + 'px';\n    }\n  }\n  //check if the bodies overlapped\n  checkOverlap() {\n    for (let i = 1; i < this.snakeBodies.length; i++) {\n      let body = this.snakeBodies[i];\n      if (this.x === body.offsetLeft && this.y === body.offsetTop) {\n        //head overlapped with body\n        throw new Error(\"The snake head hit the body!\");\n      }\n    }\n  }\n}\nexports[\"default\"] = Snake;\n\n//# sourceURL=webpack://part3/./src/modules/Snake.ts?");
 
 /***/ }),
 
@@ -26,7 +66,7 @@ eval("\n\nObject.defineProperty(exports, \"__esModule\", ({\n  value: true\n}));
   \***********************************************************************************************************/
 /***/ ((module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `* {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n}\nbody {\n  font: bold 20px \"Courier\";\n}\n.main {\n  width: 360px;\n  height: 420px;\n  background-color: #b7d4a8;\n  margin: 100px auto;\n  border: 10px solid black;\n  border-radius: 20px;\n  display: flex;\n  flex-flow: column;\n  align-items: center;\n  justify-content: space-around;\n}\n#stage {\n  width: 304px;\n  height: 304px;\n  border: 2px solid black;\n  position: relative;\n}\n#stage > #food {\n  position: absolute;\n  left: 40px;\n  top: 40px;\n  width: 10px;\n  height: 10px;\n  display: flex;\n  flex-flow: row wrap;\n  justify-content: space-between;\n  align-content: space-between;\n}\n#stage > #food > div {\n  transform: rotate(45deg);\n  width: 4px;\n  height: 4px;\n  background-color: black;\n}\n#score-board {\n  width: 300px;\n  display: flex;\n  justify-content: space-between;\n}\n#snake > div {\n  width: 10px;\n  height: 10px;\n  background-color: #000;\n  border: 1px solid #b7d4a8;\n  position: absolute;\n}\n`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://part3/./src/style/index.less?./node_modules/css-loader/dist/cjs.js!./node_modules/less-loader/dist/cjs.js");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/noSourceMaps.js */ \"./node_modules/css-loader/dist/runtime/noSourceMaps.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\");\n/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);\n// Imports\n\n\nvar ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));\n// Module\n___CSS_LOADER_EXPORT___.push([module.id, `* {\n  margin: 0;\n  padding: 0;\n  box-sizing: border-box;\n}\nbody {\n  font: bold 20px \"Courier\";\n}\n.main {\n  width: 360px;\n  height: 420px;\n  background-color: #b7d4a8;\n  margin: 100px auto;\n  border: 10px solid black;\n  border-radius: 30px;\n  display: flex;\n  flex-flow: column;\n  align-items: center;\n  justify-content: space-around;\n}\n#stage {\n  width: 304px;\n  height: 304px;\n  border: 2px solid black;\n  position: relative;\n}\n#stage > #food {\n  position: absolute;\n  left: 40px;\n  top: 40px;\n  width: 10px;\n  height: 10px;\n  display: flex;\n  flex-flow: row wrap;\n  justify-content: space-between;\n  align-content: space-between;\n}\n#stage > #food > div {\n  transform: rotate(45deg);\n  width: 4px;\n  height: 4px;\n  background-color: black;\n}\n#score-board {\n  width: 300px;\n  display: flex;\n  justify-content: space-between;\n}\n#snake > div {\n  width: 10px;\n  height: 10px;\n  background-color: #000;\n  border: 1px solid #b7d4a8;\n  position: absolute;\n}\n`, \"\"]);\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);\n\n\n//# sourceURL=webpack://part3/./src/style/index.less?./node_modules/css-loader/dist/cjs.js!./node_modules/less-loader/dist/cjs.js");
 
 /***/ }),
 
@@ -140,7 +180,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 /******/ 		};
 /******/ 	
 /******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/ 	
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -196,7 +236,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
 /******/ 	
 /******/ })()
